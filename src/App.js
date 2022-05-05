eb35e0c7fd4d23ae13ee5e259197d16ac483f02f
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-05 15:14:44 +0100

    initial socket.io config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 import './App.css';
+
+// Components
 import Nav from './components/Nav';
+import Home from './components/Home';
+import Room from './components/Socket/Room';
+import Client from './components/Socket/Client';
+
+// Socket IO Client
+import { io } from 'socket.io-client';
+
+// Context
+import { SocketContext } from './context/SocketContext';
+import { Route, Routes } from 'react-router-dom';
+const socket = io.connect('http://localhost:9090');
 
 const theme = {
 	colors: {
@@ -12,11 +25,19 @@ const theme = {
 
 function App() {
 	return (
-		<ThemeProvider theme={theme}>
-			<div className='App'>
-				<Nav></Nav>
-			</div>
-		</ThemeProvider>
+		<SocketContext.Provider value={{ socket }}>
+			<ThemeProvider theme={theme}>
+				<div className='App'>
+					<Nav></Nav>
+
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/client' element={<Client />} />
+						<Route path='/rooms/:room_name' element={<Room />} />
+					</Routes>
+				</div>
+			</ThemeProvider>
+		</SocketContext.Provider>
 	);
 }
 
