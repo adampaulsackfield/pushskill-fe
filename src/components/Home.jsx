d0da9758816eeb80ada1d059a930836af6daee08
Dane <danejwhitfield@gmail.com>
2022-05-05 18:37:56 +0100

    updated navbar and fix homepage with axios and users data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,17 @@ import { useEffect } from 'react'
 import { StyledHome } from '../styles/Home.style'
 
 const Home = ({token}) => {
-    const [name, setName] = useState('')
+    const [users, setUsers] = useState('')
 
     useEffect(() => {
         axios.get('http://localhost:9090/api/users', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
-        }).then(res => setName(res))
+        }).then(({data}) => setUsers(data.users))
     }, [])
 
-    console.log('name:', name)
+    console.log('users:', users)
 
   return (
     <StyledHome>
@@ -25,24 +25,26 @@ const Home = ({token}) => {
             </header>
             <div>
                 <h3>Here's some people we think you'll love!</h3>
-                <section>
-                    <div>
-                        <img src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip" alt="cat" />
-                        <p>Here's some info about the person you'll like</p>
-                    </div>
-                </section>
-                <section>
-                    <div>
-                        <img src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip" alt="cat" />
-                        <p>Here's some info about the person you'll like</p>
-                    </div>
-                </section>
-                <section>
-                    <div>
-                        <img src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip" alt="cat" />
-                        <p>Here's some info about the person you'll like</p>
-                    </div>
-                </section>
+                    { users && users.map(user => {
+                        return (
+                        <section>
+                            <div>
+                                <img src={user.avatarUrl} alt="cat" />
+                                <div>
+                                    <p>{user.username}</p>
+                                    <p>{user.firstName}</p>
+                                    <ul>
+                                        {user.traits.map(trait => {
+                                            return (
+                                                <li>{trait}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        </section>
+                        )
+                    })}
             </div>
         </main>
     </StyledHome>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {StyledNav} from '../styles/Nav.style'
+import {AiFillHome, AiFillInfoCircle} from 'react-icons/ai'
+import {FaUserFriends} from 'react-icons/fa'
+import {MdOutlineHelp} from 'react-icons/md'
 
 const Nav = () => {
   return (
     <StyledNav>
         <nav>
             <div>
-                <Link to='/home'>Home</Link>
-                <Link to='/partner'>Partner</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/help'>Help</Link>
-                <Link to='/faq'>FAQs</Link>
+                <Link to='/home'><AiFillHome /></Link>
+                <Link to='/partner'><FaUserFriends /></Link>
+                <Link to='/about'><AiFillInfoCircle /></Link>
+                <Link to='/faq'><MdOutlineHelp /></Link>
             </div>
         </nav>
     </StyledNav>
