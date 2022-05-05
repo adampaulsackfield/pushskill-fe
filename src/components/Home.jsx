61b313e35b32fa7d1bb76df09272ce9e84821c28
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-05 18:49:33 +0100

    import token to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,55 @@
-import axios from 'axios'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { StyledHome } from '../styles/Home.style'
+import axios from 'axios';
+import { useState, useContext, useEffect } from 'react';
+import { TokenContext } from '../context/TokenContext';
+import { StyledHome } from '../styles/Home.style';
 
-const Home = ({token}) => {
-    const [users, setUsers] = useState('')
+const Home = () => {
+	const context = useContext(TokenContext);
+	const [users, setUsers] = useState('');
 
-    useEffect(() => {
-        axios.get('http://localhost:9090/api/users', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then(({data}) => setUsers(data.users))
-    }, [])
+	useEffect(() => {
+		axios
+			.get('http://localhost:9090/api/users', {
+				headers: {
+					Authorization: `Bearer ${context.token}`,
+				},
+			})
+			.then(({ data }) => setUsers(data.users));
+	}, []);
 
-    console.log('users:', users)
+	console.log('users:', users);
 
-  return (
-    <StyledHome>
-        <main>
-            <header>
-                <h1>Welcome back {'${user}'}</h1>
-            </header>
-            <div>
-                <h3>Here's some people we think you'll love!</h3>
-                    { users && users.map(user => {
-                        return (
-                        <section>
-                            <div>
-                                <img src={user.avatarUrl} alt="cat" />
-                                <div>
-                                    <p>{user.username}</p>
-                                    <p>{user.firstName}</p>
-                                    <ul>
-                                        {user.traits.map(trait => {
-                                            return (
-                                                <li>{trait}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                            </div>
-                        </section>
-                        )
-                    })}
-            </div>
-        </main>
-    </StyledHome>
-  )
-}
+	return (
+		<StyledHome>
+			<main>
+				<header>
+					<h1>Welcome back {'${user}'}</h1>
+				</header>
+				<div>
+					<h3>Here's some people we think you'll love!</h3>
+					{users &&
+						users.map((user) => {
+							return (
+								<section>
+									<div>
+										<img src={user.avatarUrl} alt='cat' />
+										<div>
+											<p>{user.username}</p>
+											<p>{user.firstName}</p>
+											<ul>
+												{user.traits.map((trait) => {
+													return <li>{trait}</li>;
+												})}
+											</ul>
+										</div>
+									</div>
+								</section>
+							);
+						})}
+				</div>
+			</main>
+		</StyledHome>
+	);
+};
 
-export default Home
+export default Home;
