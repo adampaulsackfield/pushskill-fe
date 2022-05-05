5d2820928ff19eefcf7c7d4f76e7d9fae54b83a2
ojcorrigan <owen.corrigan5@gmail.com>
2022-05-05 12:13:13 +0100

    made the form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,41 +4,44 @@ import { StyledSignup } from '../styles/Signup.style';
 const Signup = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
 
 	return (
 		<StyledSignup>
-			<section>
-				<h2>Sign up here:</h2>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}
+			>
+				<input
+					type='text'
+					required
+					placeholder='Username:'
+					value={username}
+					onChange={(e) => {
+						setUsername(e.target.value);
 					}}
-				>
-					<label for='username' />
-					Username:
-					<input
-						type='text'
-						required
-						placeholder='Username'
-						value={username}
-						onChange={(e) => {
-							setUsername(e.target.value);
-						}}
-					/>
-					<label for='username' />
-					Password:
-					<input
-						type='text'
-						required
-						placeholder='Password'
-						value={password}
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-					/>
-					<button>Sign Up</button>
-				</form>
-			</section>
+				/>
+				<input
+					type='password'
+					required
+					placeholder='Password:'
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+				/>
+				<input
+					type='password'
+					required
+					placeholder='Confirm Password:'
+					value={confirmPassword}
+					onChange={(e) => {
+						setConfirmPassword(e.target.value);
+					}}
+				/>
+				<button>Sign Up</button>
+			</form>
 		</StyledSignup>
 	);
 };
--- src/styles/Signup.style.js
+++ src/styles/Signup.style.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
 
-export const StyledSignup = styled.section`
+export const StyledSignup = styled.form`
 	background-color: ${({ theme }) => theme.colors.mainBg};
 
 	color: ${({ theme }) => theme.colors.mainText};
+	width: 40vw;
+	height: 300px;
 	margin: 0;
-	padding: 0;
-	width: 50vw;
-
-	h2 {
-		margin: 20px;
-	}
+	padding-top: 20px;
 
 	form {
-		margin: 20px;
-		width: 50%;
+		width: 80%;
+		margin: auto;
 		display: flex;
 		flex-direction: column;
+		align-items: center;
+	}
+
+	input {
+		margin: 10px;
+		height: 40px;
+		width: 90%;
+		border-radius: 5px;
+	}
+
+	button {
+		margin-top: 20px;
+		width: 200px;
+		height: 40px;
+		border-radius: 10px;
 	}
 `;
