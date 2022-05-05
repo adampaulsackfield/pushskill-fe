a4cd4022acd904644823da3f5935892e65387bc1
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-05 18:02:03 +0100

    variable renamed

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -18,43 +18,45 @@ const Signup = () => {
 
 	return (
 		<StyledSignup>
-    	<main>
+			<main>
 				<h2>Sign up</h2>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-          signUpUser(username, password).then((userData) => {
-						console.log(userData + 'userdata');
-					});
-				}}
-			>
-				<input
-					name='username'
-					type='text'
-					required
-					placeholder='Username:'
-					value={signUpForm.username}
-					onChange={(e) => handleInputChange(e)}
-				/>
-				<input
-					name='password'
-					type='password'
-					required
-					placeholder='Password:'
-					value={signUpForm.password}
-					onChange={(e) => handleInputChange(e)}
-				/>
-				<input
-					name='confirmPassword'
-					type='password'
-					required
-					placeholder='Confirm Password:'
-					value={signUpForm.confirmPassword}
-					onChange={(e) => handleInputChange(e)}
-				/>
-				<button>Sign Up</button>
-			  </form>
-      </main>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						signUpUser(signUpForm.username, signUpForm.password).then(
+							(userData) => {
+								console.log(userData + 'userdata');
+							}
+						);
+					}}
+				>
+					<input
+						name='username'
+						type='text'
+						required
+						placeholder='Username:'
+						value={signUpForm.username}
+						onChange={(e) => handleInputChange(e)}
+					/>
+					<input
+						name='password'
+						type='password'
+						required
+						placeholder='Password:'
+						value={signUpForm.password}
+						onChange={(e) => handleInputChange(e)}
+					/>
+					<input
+						name='confirmPassword'
+						type='password'
+						required
+						placeholder='Confirm Password:'
+						value={signUpForm.confirmPassword}
+						onChange={(e) => handleInputChange(e)}
+					/>
+					<button>Sign Up</button>
+				</form>
+			</main>
 		</StyledSignup>
 	);
 };
