0c39bba8101b83fa1e5eec5824dd38b818f65361
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-05 15:38:17 +0100

    removed unused Link import

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyledNav } from '../styles/Nav.style';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
 	return (
