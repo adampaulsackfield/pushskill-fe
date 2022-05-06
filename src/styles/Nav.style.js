6af698e38798d82b5355a52b7cc40af865ad742b
Dane <danejwhitfield@gmail.com>
2022-05-06 19:49:36 +0100

    reworked navbar, more responsive and easier to see content now

--- src/styles/Nav.style.js
+++ src/styles/Nav.style.js
@@ -6,7 +6,7 @@ export const StyledNav = styled.nav`
 	left: 50%;
 	transform: translateX(-50%);
 	background-color: ${({ theme }) => theme.colors.mainBg};
-	height: 80px;
+	height: 60px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -14,6 +14,7 @@ export const StyledNav = styled.nav`
 	border-radius: 50px;
 	padding: 10px;
 	z-index: 1000;
+	opacity: 0.6;
 
 	div {
 		display: flex;
@@ -28,14 +29,20 @@ export const StyledNav = styled.nav`
 		align-items: center;
 		background-color: ${({ theme }) => theme.colors.darkerBg};
 		border-radius: 50%;
-		width: 60px;
-		height: 60px;
+		width: 40px;
+		height: 40px;
 		text-decoration: none;
 		color: ${({ theme }) => theme.colors.mainText};
 		transition: all 0.3s ease;
+		font-size: 28px;
 	}
 
 	a:hover {
+		background-color: #000;
+	}
+
+	a:active,
+	a:focus {
 		background-color: ${({ theme }) => theme.colors.mainAccent};
 	}
 `;
