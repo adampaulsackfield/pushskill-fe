cebe5966f0e630d3bd81600c952e5041df38eeb7
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-06 12:49:05 +0100

    fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -22,11 +22,9 @@ import { SocketContext } from './context/SocketContext';
 
 import { TokenContext } from './context/TokenContext';
 
-
 import { UserContext } from './context/UserContext';
 
 import { RoomsContext } from './context/RoomsContext';
-
 
 const socket = io.connect('http://localhost:9090');
 
@@ -45,43 +43,31 @@ const theme = {
 function App() {
 	const [token, setToken] = useState('');
 	const [user, setUser] = useState('');
-  const [rooms, setRooms] = useState([]);
-  
-	return (
-		<SocketContext.Provider value={{ socket }}>
-			<UserContext.Provider value={{ user, setUser }}>
-				<TokenContext.Provider value={{ token, setToken }}>
-					<ThemeProvider theme={theme}>
-						<Global />
-						<Nav />
-
-
-
+	const [rooms, setRooms] = useState([]);
 
 	return (
 		<SocketContext.Provider value={{ socket }}>
 			<TokenContext.Provider value={{ token, setToken }}>
-        <UserContext.Provider value={{ user, setUser }}>
-				<RoomsContext.Provider value={{ rooms, setRooms }}>
-					<ThemeProvider theme={theme}>
-						<Global />
-						<Nav />
+				<UserContext.Provider value={{ user, setUser }}>
+					<RoomsContext.Provider value={{ rooms, setRooms }}>
+						<ThemeProvider theme={theme}>
+							<Global />
+							<Nav />
 
-						<Routes>
-							<Route path='/home' element={<Home />} />
-							<Route path='/client' element={<Client />} />
-							<Route path='/rooms/:room_name' element={<Room />} />
-							<Route path='/signup' element={<Signup />} />
-							<Route path='/partner' element={<Partner />} />
-							<Route path='/login' element={<Login />} />
-							<Route path='/profile/:user_id' element={<Profile />} />
-						</Routes>
-						<ToastContainer position='top-center' />
-					</ThemeProvider>
-				</RoomsContext.Provider>
-		    </UserContext.Provider>
+							<Routes>
+								<Route path='/home' element={<Home />} />
+								<Route path='/client' element={<Client />} />
+								<Route path='/rooms/:room_name' element={<Room />} />
+								<Route path='/signup' element={<Signup />} />
+								<Route path='/partner' element={<Partner />} />
+								<Route path='/login' element={<Login />} />
+								<Route path='/profile/:user_id' element={<Profile />} />
+							</Routes>
+							<ToastContainer position='top-center' />
+						</ThemeProvider>
+					</RoomsContext.Provider>
+				</UserContext.Provider>
 			</TokenContext.Provider>
-
 		</SocketContext.Provider>
 	);
 }
