ac8c762a4f7946722cfdbc82e86ec481ca21ae0c
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-06 12:39:47 +0100

    stack trace

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,57 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { StyledPartner } from '../styles/Partner.style'
-import {useContext} from 'react'
-import {TokenContext} from '../context/TokenContext'
-import { getMessages, getRooms, createMessage } from '../utils/api'
-import { RoomsContext } from '../context/RoomsContext'
-import { SocketContext } from '../context/SocketContext'
+import React, { useEffect, useState } from 'react';
+import { StyledPartner } from '../styles/Partner.style';
+import { useContext } from 'react';
+import { TokenContext } from '../context/TokenContext';
+import { getMessages, getRooms, createMessage } from '../utils/api';
+import { RoomsContext } from '../context/RoomsContext';
+import { SocketContext } from '../context/SocketContext';
 
 const Partner = () => {
-    const context = useContext(TokenContext)
-    const token = context.token
-    const [messages, setMessages] = useState([])
-    const [message, setMessage] = useState('')
-	const {rooms} = useContext(RoomsContext)
-    const {socket} = useContext(SocketContext)
+	const context = useContext(TokenContext);
+	const token = context.token;
+	const [messages, setMessages] = useState([]);
+	const [message, setMessage] = useState('');
+	const { rooms } = useContext(RoomsContext);
+	const { socket } = useContext(SocketContext);
 
-    useEffect(() => {
-        socket.on('receive_message', (data) => {
-            console.log('data:', data)
-            setMessages(data)
-        })
-    }, [])
+	useEffect(() => {
+		socket.on('receive_message', (data) => {
+			console.log('data:', data);
+			setMessages(data);
+		});
+	}, []);
 
-    console.log('rooms:', rooms)
+	console.log('rooms:', rooms);
 
-    const room_id = rooms._id
+	const room_id = rooms._id;
 
-    console.log('room_id:', room_id)
-    const recipientId = '6272ede4dcf5f9c3dba8b056'
+	console.log('room_id:', room_id);
+	const recipientId = '6272ede4dcf5f9c3dba8b056';
 
-    const handleSendMsg = (e) => {
-        e.preventDefault()
-        socket.emit('chat_message', {token, room_id, recipientId, message})
-    }
+	const handleSendMsg = (e) => {
+		e.preventDefault();
+		socket.emit('chat_message', {
+			token,
+			roomId: '6272ee2109c3dd977e39ab9c',
+			recipientId,
+			message,
+		});
+	};
 
-    console.log('rooms:', rooms)
+	console.log('rooms:', rooms);
 
-    console.log('messages:', messages)
+	console.log('messages:', messages);
 
-  return (
-    <StyledPartner>
-        <main>
-            <section>
-            <h1>{rooms ? rooms._id : 'No room'}</h1>
-                <div>
-                    <form>
-                        <input type="text" placeholder='say something...' onChange={(e) => setMessage(e.target.value)} />
-                        <button onClick={handleSendMsg}>SEND</button>
-                    </form>
-                </div>
-            </section>
-        </main>
-    </StyledPartner>
-  )
-}
+	return (
+		<StyledPartner>
+			<main>
+				<section>
+					<h1>{rooms ? rooms._id : 'No room'}</h1>
+					<div>
+						<form>
+							<input
+								type='text'
+								placeholder='say something...'
+								onChange={(e) => setMessage(e.target.value)}
+							/>
+							<button onClick={handleSendMsg}>SEND</button>
+						</form>
+					</div>
+				</section>
+			</main>
+		</StyledPartner>
+	);
+};
 
-export default Partner
+export default Partner;
