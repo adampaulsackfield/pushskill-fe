21fb870bfaeaf75de12ef58ec415c64e9c79e8c3
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-10 17:46:13 +0100

    refactor nav: dynamic links depending on login status

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 import { FaUserFriends } from 'react-icons/fa';
 import { MdOutlineHelp } from 'react-icons/md';
+import { AiOutlineUserAdd, AiOutlineLogin } from 'react-icons/ai';
 
 // Theme
 import { StyledNav } from '../styles/Nav.style';
 
 // Context
 import { UserContext } from '../context/UserContext';
+import { TokenContext } from '../context/TokenContext';
 
 const Nav = () => {
 	const { userId } = useContext(UserContext);
+	const { token } = useContext(TokenContext);
 
 	return (
 		<StyledNav>
@@ -20,12 +23,26 @@ const Nav = () => {
 					<Link to='/home'>
 						<AiFillHome />
 					</Link>
-					<Link to='/partner'>
-						<FaUserFriends />
-					</Link>
-					<Link to={`/profile/${userId}`}>
-						<AiFillHome />
-					</Link>
+					{token ? (
+						<>
+							<Link to='/partner'>
+								<FaUserFriends />
+							</Link>
+							<Link to={`/profile/${userId}`}>
+								<AiFillHome />
+							</Link>
+						</>
+					) : (
+						<>
+							<Link to='/signup'>
+								<AiOutlineUserAdd />
+							</Link>
+							<Link to={`/login/`}>
+								<AiOutlineLogin />
+							</Link>
+						</>
+					)}
+
 					<Link to='/about'>
 						<AiFillInfoCircle />
 					</Link>
