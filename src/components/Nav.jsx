18e26e8fd67792cb09a6b826b4821e323969cebd
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-10 17:58:17 +0100

    added logout button to clear state, only visible for logged in users

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 import { FaUserFriends } from 'react-icons/fa';
 import { MdOutlineHelp } from 'react-icons/md';
-import { AiOutlineUserAdd, AiOutlineLogin } from 'react-icons/ai';
+import {
+	AiOutlineUserAdd,
+	AiOutlineLogin,
+	AiOutlineLogout,
+} from 'react-icons/ai';
 
 // Theme
 import { StyledNav } from '../styles/Nav.style';
@@ -11,10 +15,19 @@ import { StyledNav } from '../styles/Nav.style';
 // Context
 import { UserContext } from '../context/UserContext';
 import { TokenContext } from '../context/TokenContext';
+import { toast } from 'react-toastify';
 
 const Nav = () => {
 	const { userId } = useContext(UserContext);
 	const { token } = useContext(TokenContext);
+
+	const handleLogoutUser = () => {
+		localStorage.removeItem('id');
+		localStorage.removeItem('token');
+		localStorage.removeItem('roomId');
+
+		toast.success('Logout Successful');
+	};
 
 	return (
 		<StyledNav>
@@ -30,6 +43,9 @@ const Nav = () => {
 							</Link>
 							<Link to={`/profile/${userId}`}>
 								<AiFillHome />
+							</Link>
+							<Link to='#' onClick={handleLogoutUser}>
+								<AiOutlineLogout />
 							</Link>
 						</>
 					) : (
